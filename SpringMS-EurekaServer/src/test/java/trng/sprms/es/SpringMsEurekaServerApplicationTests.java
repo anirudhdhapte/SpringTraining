0dd87ee788831fdadb3fd86dc9c2dde5998babbd
ANI <ANI@ANI-PC>
2022-03-24 12:08:00 +0530

    1st commit

---
+++ SpringMS-EurekaServer/src/test/java/trng/sprms/es/SpringMsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package trng.sprms.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
