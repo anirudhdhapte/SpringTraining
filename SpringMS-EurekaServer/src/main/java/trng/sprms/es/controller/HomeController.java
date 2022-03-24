285f6e27345f52ac5537eb86e5fba5512881ec89
ANI <ANI@ANI-PC>
2022-03-24 12:12:18 +0530

    before test commit

--- SpringMS-EurekaServer/src/main/java/trng/sprms/es/controller/HomeController.java
+++ SpringMS-EurekaServer/src/main/java/trng/sprms/es/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/home")
 	public String homepage() {
-		return "Hello There!";
+		return "Before test Commit";
 	}
 }
